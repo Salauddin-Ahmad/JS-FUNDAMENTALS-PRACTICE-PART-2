3ee478567c56540330dac738fcd67489494571a0
Md. Salauddin <salauddinahmed555@gmail.com>
2024-10-15 21:58:31 +0600

    removed some wrong syntax

--- lectures.js
+++ lectures.js
@@ -16,12 +16,9 @@
 // console.log('123' < 57)
 // copied from termux terminal
 
-> console.log(Boolean('123' < 57));
-false
-undefined
-> console.log(Boolean('12' < 57));
-true
-undefined
->
+ console.log(Boolean('123' < 57));
+ //false //undefined
+ console.log(Boolean('12' < 57));
+//true //undefined
 
 // console.log(5 + 6 + '4' + 9 - 4 - 2)
