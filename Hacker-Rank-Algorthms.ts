4a2965b3efed90316eee7af82e5d2f57b1e2cf53
Md. Salauddin <155472442+Salauddin-Ahmad@users.noreply.github.com>
2024-12-29 00:36:46 +0600

    Solved the starter one :tada:

--- Hacker-Rank-Algorthms.ts
+++ Hacker-Rank-Algorthms.ts
@@ -3,28 +3,23 @@
 --------------------------------------------------------------
 --------------------------------------------------------------
 
+
+function solveMeFirst(a: number, b: number): number {
+    const sum = a + b
+    return sum
+} 
+
+
 function main(){
-   const sum =  solveMeFirst(7, 3);
+   const a = parseInt(readline());
+   const b = parseInt(readline());
+   
+   const sum =  solveMeFirst(a, b);
 
    console.log(solveMeFirst)
 
 }
 
-
-
-
-function solveMeFirst(a: number, b: number): number {
-if (1 <= a && b <= 1000){
-    const sum = a + b
-    return a + b
-} else {
-    throw new Error('input values are must be within 1 to 1000')
-}
-
-
-}
-
-main()
 
 --------------------------------------------------------------
 --------------------------------------------------------------
